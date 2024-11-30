c7e7069b31eaa8c4f4d68c5a33221ddf0d9ace6b
Steve-B17 <steve.joel.cbe@gmail.com>
2024-11-30 11:24:43 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Home from '.Components/home';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar'
+const home = () => {
+  return (
+    <div class="home-container">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default home
